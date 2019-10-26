7e233281b0159fd452f19e25fbf27362e2846a10
Andres Felipe Valencia Benitez <afvalenciab@gmail.com>
2019-10-26 13:25:17 -0500

    Se eliminan las carpetas y archivos de la maquetacion

--- src/pages/LandingPage/LayoutLanding.jsx
+++ src/pages/LandingPage/LayoutLanding.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import LandingPage from './LandingPage';
-import Modal from '../../containers/Modal';
-import Signin from '../../containers/Signin';
-import Register from '../../containers/Register';
 
 const LayoutLanding = (props) => {
-  return (
-    <>
-      <LandingPage props={props} />
-      {/* <Modal>
-        <Signin />
-        <Register />
-      </Modal> */}
-    </>
-  );
+  return <LandingPage props={props} />;
 };
 
 export default LayoutLanding;
