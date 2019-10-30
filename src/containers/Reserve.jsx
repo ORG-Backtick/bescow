0848fd7585131298cdd96b862491a6fbe0e0bb6e
Andres Felipe Valencia Benitez <afvalenciab@gmail.com>
2019-10-30 05:54:01 -0600

    Se crea el componente para generar la reserva del coworking

---
+++ src/containers/Reserve.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../assets/styles/components/Reserve.scss';
+import '../assets/styles/Icons.css';
+
+const Reserve = (props) => {
+
+  return (
+    <div className='overlay__reserve'>
+      <div className='reserve__container'>
+        <section className='reserve'>
+          <div className='close'>
+            <i className='icon-close icon' onClick={props.handleCloseClick} />
+          </div>
+          <h1 className='title__reserve'>Confirmar Reserva</h1>
+          <p className='title__fare-reserve'>
+            $ 180 
+            <span> MXN/día</span>
+          </p>
+          <hr className='line' />
+          <label className='label__title-reserve' >Fechas</label>
+          <div className='date__detail-reserve'>
+            <span>26/10/2019</span>
+            <i>----</i>
+            <span>27/10/2019</span>
+          </div>
+          <label className='label__title-reserve'>Coworkers</label>
+          <div className='coworkers'>
+            <span>1 coworker</span>
+          </div>
+          <div className='summary__reserve'>
+            <div><span>$180 x 1 día</span></div>
+            <div><span>$180</span></div>
+          </div>
+          <hr className='line' />
+          <div className='total__reserve'>
+            <p>Total:</p>
+            <p>$180</p>
+          </div>
+          <button type='button' className='button button__reserve'>
+            Reservar
+          </button>
+          <p className='cancel__reserve'>Cancelar</p>
+        </section>
+      </div>
+    </div>
+  );
+};
+export default Reserve;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LayoutSearch from '../pages/SearchPage/LayoutSearch';
 import LayoutLanding from '../pages/LandingPage/LayoutLanding';
+import Reserve from '../containers/Reserve';
 import '../assets/styles/App.scss';
 
 const Home = () => {
@@ -10,6 +11,8 @@ const Home = () => {
       <Switch>
         <Route exact path='/' component={LayoutLanding} />
         <Route exact path='/search' component={LayoutSearch} />
+
+        <Route exact path='/reserve' component={Reserve} />
       </Switch>
     </BrowserRouter>
   );
