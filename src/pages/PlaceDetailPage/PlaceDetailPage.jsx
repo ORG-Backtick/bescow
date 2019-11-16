bb1fe4022705035d1d99447ac14e4196b89456ee
lisset-orozco <liss612@gmail.com>
2019-11-16 11:00:13 -0600

    improvement

--- src/pages/PlaceDetailPage/PlaceDetailPage.jsx
+++ src/pages/PlaceDetailPage/PlaceDetailPage.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import moment from 'moment';
-
 import Header from '../../components/Header';
 import DetailsList from './componentes/DetailsList';
 import Footer from '../../components/Footer';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -106,14 +106,16 @@ const reducer = (state = initialState, action) => {
     case 'SET_COST_DETAIL': {
       const { cow, filterList } = action.payload;
       let dateCount = 1;
-      if (filterList.formDateCheckin) {
+
+      if (filterList && filterList.formDateCheckin) {
         const date1 = moment(filterList.formDateCheckin);
         const date2 = moment(filterList.formDateCheckout);
 
         dateCount = (date2.diff(date1, 'days') + 1);
       } else {
-        filterList.formDateCheckin = moment().add(1, 'days').format('DD-MM-YYYY');
-        filterList.formDateCheckout = moment().add(1, 'days').format('DD-MM-YYYY');
+        const dayDefault = moment().add(1, 'days').format('DD-MM-YYYY');
+        filterList['formDateCheckin'] = dayDefault;
+        filterList['formDateCheckout'] = dayDefault;
       }
 
       const cost = dateCount * cow.dayFare * filterList.formCow;
